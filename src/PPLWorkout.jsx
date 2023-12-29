094f70811f68a7c6ab40b3581ccdd3750772bc4d
JCSweet <45698957+JCSweet@users.noreply.github.com>
2023-12-29 14:43:34 -0700

    Added exercises and basic instructions
    Added more exercises to list. Also added basic instructions (placeholder, will likely update later) to start workout when no workout is active, to avoid a blank screen.

--- src/PPLWorkout.jsx
+++ src/PPLWorkout.jsx
@@ -6,6 +6,7 @@ export default function PPLWorkout({exercises, updateSet}) {
     <>
       {/* <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}> */}
       <List sx={{ width: "100%", maxWidth: 360, bgcolor: "#333333" }}>
+        {exercises.length === 0 ? <div className="noWorkoutMessage"><h2>Click "Quick PPL" <br />to begin!</h2></div> : null}
         {exercises.map((exercise) => (
           <PPLWorkoutItem
             exercise={exercise}
